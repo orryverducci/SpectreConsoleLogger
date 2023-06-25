3ffe58dfe4bfb1c51ff010f6b9ee99be4e1e72e9
Orry Verducci <orry@orryverducci.co.uk>
2023-06-25 15:11:35 +0100

    Add documentation comments for Style

--- src/Library/Style.cs
+++ src/Library/Style.cs
@@ -1,5 +1,11 @@
 namespace SpectreConsoleLogger;
 
+/// <summary>
+/// Logging styles that can be used by the logger.
+/// </summary>
+/// <remarks>
+/// The extended logging style logs the category (usually the class name and namespace) and the timezone.
+/// </remarks>
 public enum Style
 {
     Standard,
